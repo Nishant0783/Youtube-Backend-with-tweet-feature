ea37a979ab8fdbe30239bd85ff4ae07e7f01a9ce
Nishant Paliwal <paliwalnishant2003@gmail.com>
2024-01-11 20:28:04 +0530

    Backend till 11/01/24. Completion of configuration.

---
+++ Backend03/src/middlewares/multer.middleware.js
@@ -0,0 +1,37 @@
+// Multer is a middleware which is used to get the file from user and save it to localstorage or where ever we need.
+
+// To save file to local storage we will use "diskStorage()" method of multer which will save it to HDD. We have another method which is "memoryStorage()" which is used to file to RAM but this method is not advisable because if a large file comes then memory will get full.
+// "diskStorage()" accpet object in which:-
+//  1. destination - function to determine the storage path for storing the file. We have 3 paramters there which are "request(req)" which has the json data which can be handled by express, then we have "file(file)" which gives us access to file which is why multer is used and last is "callback(cb)" function which accepts two arguments which are "error" and second one is "path to file" where we will keep uploaded files.
+// 2. filename - function to determine the name of the uploaded file. Here also we get "req, file, cb" as paramteres. "cb" is used to actually save the file name. We can give anything as file name but industries prefer to give some uuid's or nanoid's. Here "uniqueSuffix" is use to generate a suffix for the file name and add it to the file name. In "cb" we have a "file" which has different options through we can access all the information about the file and actually we can log this "file" to get the data about the file. But in our project we will go for simpler appraoch of naming file, so I have commented the code given by multer and will use our own.
+/***This whole code is taken from multer's github page under "diskStorage" section. */
+
+import multer from 'multer';
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // In this cb we are passing 1st argument as null because we are not dealing with errors initially.
+        // When we will need we will modify it.
+        cb(null, './publicc/temp')
+    },
+    filename: function (req, file, cb) {
+    /* Code given by multer.
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    */
+        cb(null, file.originalname);
+        // We are currently storing files with original name. It might have a problem that 1 or more files have same name and new file can override the old file.
+        // But we have a margin of error which is time. For very-very minute time we have files on our server so we can play with this. In industry we will folow industrial approach.
+    }
+
+
+})
+
+export const upload = multer(
+    { 
+        // storage: storage 
+        storage, 
+        // Since we are in ES6, we have this leverage that if the key and value have same name then we can write any one of them.
+    }
+)
---
+++ Backend03/src/utils/cloudinary.js
@@ -0,0 +1,45 @@
+// In this file we will make a utility to upload file to cloudinar.
+// The work we have to do is:
+//     ---> Get file from local storage, upload it to cloudinary and then remove file from local storage because once it is uploaded to cloud then we don;'t need it in local.
+
+import {v2 as cloudinary} from 'cloudinary';
+
+// fs: fs is fileSystem anda we are using it to remove file from local storage.
+// It is provided by nodejs.
+import fs from 'fs';
+// We will use "unlinkSync()" method to remove file.
+// Go tho notes.txt to read more about it.
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        // Check if we don't have local file path the we will return null or a message ("Cannot find path").
+        if(!localFilePath) return null
+
+        // upload file on clodinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",  // Using this option we can give the type of resource we are uploding like image, raw. Using "auto" it will automatically detect.
+        });
+        // file has been uploaded successfully
+        console.log(`file uploaded on Cloudinary successfully: ${response.url}`);
+        // response.url will give the public url of the uploaded file.
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // This operation is used to remove the file.
+        // In the start of this page I have commented that we will remove file if it get's uploaded on cloud but we are removing file in catch block and the code jumps to catch block only if there is some error in try block and in try block we uploding file on cloud.
+        // The question is if file is not uploaded then why are removing it here?
+        // This is because there might be high chances that the file is malicious and that's why it is not uploaded to cloud. Keeping malicious file in local is suicide. Therefore, we remove file even if it is not uploaded.
+        
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
+
+          
